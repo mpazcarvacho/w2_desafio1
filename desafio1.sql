f3474a879a9d2c014f4269a719d329e3c91b6620
mariapaz <mpazcarvacho@gmail.com>
2020-06-28 01:04:54 -0400

    archivo sql

---
+++ desafio1.sql
@@ -0,0 +1,36 @@
+--Semana2 - Desafío 1 - Modelamiento de datos
+
+CREATE DATABASE s1d1;
+
+\c s1d1
+
+CREATE TABLE departamentos(
+    id SERIAL,
+    nombre VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE trabajadores(
+    id SERIAL,
+    rut VARCHAR(14) NOT NULL,
+    nombre VARCHAR(250) NOT NULL,
+    direccion VARCHAR(300),
+    id_departamento INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_departamento) REFERENCES departamentos(id)
+);
+
+CREATE TABLE liquidaciones(
+    id SERIAL,
+    url_archivo VARCHAR(300) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE liquidaciones_trabajadores(
+    id SERIAL,
+    id_trabajador SMALLINT,
+    id_liquidacion SMALLINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_trabajador) REFERENCES trabajadores(id),
+    FOREIGN KEY (id_liquidacion) REFERENCES liquidaciones(id)
+);
